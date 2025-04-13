9a7252cedba70bdc6f226aaaff8f6562040d2ef8
Tim Jentzsch <commits@timjen.net>
2025-04-13 16:38:49 +0200

    Set up colors and initial global styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	// Global styles
+	import '../styles/palette.css';
+	import '../styles/colors.css';
+	import '../styles/global.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
