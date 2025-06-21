28785c224f09940fb102402b9c9a828f48a272e6
Tim Jentzsch <commits@timjen.net>
2025-06-21 20:40:39 +0200

    Add overview of all posts

---
+++ src/routes/blog/+page.server.ts
@@ -0,0 +1,27 @@
+import type { PageServerLoad } from './$types';
+import { slugFromPath } from '$lib/slugFromPath';
+import type { BlogPost, MdsvexFile } from '$lib/blog';
+
+const MAX_POSTS = 10;
+
+export const load: PageServerLoad = async () => {
+	const modules = import.meta.glob(`/src/blog/*.{md,svx,svelte.md}`);
+
+	const postPromises = Object.entries(modules).map(([path, resolver]) =>
+		resolver().then(
+			(post) =>
+				({
+					slug: slugFromPath(path),
+					...(post as unknown as MdsvexFile).metadata
+				}) as BlogPost
+		)
+	);
+
+	const posts = await Promise.all(postPromises);
+	const publishedPosts = posts
+		.filter((post) => post.published)
+		.slice(0, MAX_POSTS)
+		.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
+
+	return { posts: publishedPosts };
+};
