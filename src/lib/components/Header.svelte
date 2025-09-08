ef40132c7884df61834b1beb552ab0ff3dce41a4
Tim Jentzsch <commits@timjen.net>
2025-09-08 22:24:06 +0200

    Add link to GitHub

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import HeaderIcon from './HeaderIcon.svelte';
 	import HeaderLink from './HeaderLink.svelte';
 
 	interface Props {
@@ -11,7 +12,17 @@
 <header>
 	<a id="websiteName" href="/">timjen</a>
 	<nav>
-		<HeaderLink href="/blog" {path}>blog</HeaderLink>
+		<div class="links">
+			<HeaderLink href="/blog" {path}>blog</HeaderLink>
+		</div>
+		<div class="icons">
+			<HeaderIcon
+				href="https://github.com/TimJentzsch"
+				alt="GitHub"
+				srcDark="/icons/github-dark.svg"
+				srcLight="/icons/github-light.svg"
+			/>
+		</div>
 	</nav>
 </header>
 
@@ -38,6 +49,15 @@
 	}
 
 	nav {
+		display: flex;
+		flex-flow: row;
+		justify-content: space-between;
+		gap: var(--spacing2);
+		flex-grow: 1;
+	}
+
+	.links,
+	.icons {
 		display: flex;
 		flex-flow: row;
 		gap: var(--spacing1);
---
+++ src/lib/components/HeaderIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Props {
+		srcDark: string;
+		srcLight?: string;
+		href: string;
+		alt: string;
+	}
+	const { srcLight, srcDark, alt, href }: Props = $props();
+</script>
+
+<a {href} target="_blank" rel="noopener noreferrer">
+	<picture>
+		{#if srcLight}
+			<source srcset={srcLight} media="(prefers-color-scheme: light)" />
+		{/if}
+		<img src={srcDark} {alt} />
+	</picture>
+</a>
+
+<style>
+	img {
+		height: 1em;
+	}
+</style>
