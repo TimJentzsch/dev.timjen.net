de2c9623c096fad06b3d32a8625a22f9d40be71c
Tim Jentzsch <commits@timjen.net>
2025-11-08 11:06:59 +0100

    Refactor loading of posts into reusable function

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,3 +1,5 @@
+import { slugFromPath } from './slugFromPath';
+
 export interface MdsvexFile {
 	default: import('svelte').Component;
 	metadata: Record<string, string>;
@@ -12,4 +14,37 @@ export interface BlogPost {
 	description: string;
 	date: string;
 	published: boolean;
+}
+
+/**
+ * @param url The URL of the current request.
+ * @param maxPosts The maximum number of posts to load.
+ * @returns The blog posts, sorted by date (newest first).
+ */
+export async function loadPosts(url: URL, maxPosts: number = 10): Promise<BlogPost[]> {
+	const modules = import.meta.glob(`/src/blog/*.{md,svx,svelte.md}`);
+
+	const postPromises = Object.entries(modules).map(([path, resolver]) =>
+		resolver().then(
+			(post) =>
+				({
+					slug: slugFromPath(path),
+					...(post as unknown as MdsvexFile).metadata
+				}) as BlogPost
+		)
+	);
+
+	const posts = await Promise.all(postPromises);
+	return (
+		posts
+			// Include unpublished posts in dev mode
+			.filter((post) => post.published || showDrafts(url))
+			.slice(0, maxPosts)
+			.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1))
+	);
+}
+
+/** @returns `true` if draft posts should be shown to the user, else `false`. */
+export function showDrafts(url: URL): boolean {
+	return import.meta.env.DEV || url.searchParams.has('showDrafts');
 }
--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,29 +1,8 @@
 import type { PageServerLoad } from './$types';
-import { slugFromPath } from '$lib/slugFromPath';
-import type { BlogPost, MdsvexFile } from '$lib/blog';
-import { showDrafts } from '$lib/showDrafts';
-
-const MAX_POSTS = 10;
+import { loadPosts } from '$lib/blog';
 
 export const load: PageServerLoad = async ({ url }) => {
-	const modules = import.meta.glob(`/src/blog/*.{md,svx,svelte.md}`);
+	const posts = await loadPosts(url);
 
-	const postPromises = Object.entries(modules).map(([path, resolver]) =>
-		resolver().then(
-			(post) =>
-				({
-					slug: slugFromPath(path),
-					...(post as unknown as MdsvexFile).metadata
-				}) as BlogPost
-		)
-	);
-
-	const posts = await Promise.all(postPromises);
-	const publishedPosts = posts
-		// Include unpublished posts in dev mode
-		.filter((post) => post.published || showDrafts(url))
-		.slice(0, MAX_POSTS)
-		.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
-
-	return { posts: publishedPosts };
+	return { posts };
 };
